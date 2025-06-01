aff3adfbc136d91642d34fba0a49a40f02255feb
oredoo <muhammad.123140197@student.itera.ac.id>
2025-06-01 23:33:24 +0700

    Add files via upload

---
+++ run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Greedy --email=test@email.com --name=momogi --password=123456 --team etimo"
+start cmd /c "python main.py --logic Greedy --email=test1@email.com --name=momogi2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Greedy --email=test2@email.com --name=momogi3 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Greedy --email=test3@email.com --name=momogi4 --password=123456 --team etimo"
